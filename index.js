222378cf35b4c2b567d4094469201a6480c09d30
hadiwq <98136543+hadiwq@users.noreply.github.com>
2022-10-26 14:13:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -12,17 +12,16 @@
   </body>
 </html>
 
-<script>
-
-<html>
+html>
 <body>
 
-<h2>What Can JavaScript Do?</h2>
+<h2>My First JavaScript</h2>
 
-<p id="demo">JavaScript can change HTML content.</p>
+<button type="button"
+onclick="document.getElementById('demo').innerHTML = Date()">
+Click me to display Date and Time.</button>
 
-<button type="button" onclick='document.getElementById("demo").innerHTML = "Hello JavaScript!"'>Click Me!</button>
+<p id="demo"></p>
 
 </body>
-</html>
-</script>
+</html> 
