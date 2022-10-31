4cee99692553a4db7b8fb20a6325e01d0a3444ce
hadiwq <98136543+hadiwq@users.noreply.github.com>
2022-10-31 14:29:46 -0500

    Update index.php

--- index.php
+++ index.php
@@ -15,6 +15,8 @@
 html>
 <body>
 
+  function myFunction() {
+
 <h2>My First JavaScript</h2>
 
 <button type="button"
@@ -22,6 +24,6 @@ onclick="document.getElementById('demo').innerHTML = Date()">
 Click me to display Date and Time.</button>
 
 <p id="demo"></p>
-
+}
 </body>
 </html> 
