21752b50cf4f5c347ed2956c1fb3087657daffaa
hadiwq <98136543+hadiwq@users.noreply.github.com>
2022-11-07 13:49:07 -0600

    Update index.php

--- index.php
+++ index.php
@@ -28,8 +28,7 @@ Click me to display Date and Time.</button>
   
   function myFunction2() {
   <button type="button"
-   onclick = "document.getElementById("demo").innerHTML = myFunction2()">
-  return 5 * 5;
+  return 5 * 5;>
 }
   
 </body>
