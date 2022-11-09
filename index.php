fa40cc4e65342c40e0844817c2a24fed585597b0
hadiwq <98136543+hadiwq@users.noreply.github.com>
2022-11-08 19:51:45 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <html>
 <body>
+  
+  h2>My First JavaScript</h2>
+
+<button type="button"
+onclick="document.getElementById('demo').innerHTML = Date()">
+Click me to display Date and Time.</button>
+
+<p id="demo"></p>
 
 <h2>JavaScript Math</h2>
 
