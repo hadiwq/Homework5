dd36ee9e79cd9f498c3644b335484beacc9beaa9
hadiwq <98136543+hadiwq@users.noreply.github.com>
2022-11-08 19:39:42 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,46 +1,4 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-  </head>
-  <body>
-    <h1>Hello, world!</h1>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-
-
-  function myFunction() {
-
-<h2>My First JavaScript</h2>
-
-<button type="button"
-onclick="document.getElementById('demo').innerHTML = Date()">
-Click me to display Date and Time.</button>
-
-<p id="demo"></p>
-}
-  
-  function myFunction2() {
-  <h2>JavaScript Math.PI</h2>
-
-<p>Math.PI returns the ratio of a circle's circumference to its diameter:</p>
-
-<p iid="demo"></p>
-
-<script>
-document.getElementById("demo").innerHTML = Math.PI;
-</script>
-
-}
-  
-  function myFunction3() {
-  Math.random();
-  
-  }
-    
-    <html>
+<html>
 <body>
 
 <h2>JavaScript Math</h2>
@@ -57,6 +15,3 @@ Math.floor(Math.random() * 10);
 
 </body>
 </html>
-
-</body>
-</html> 
