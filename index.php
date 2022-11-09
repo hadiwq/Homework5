d72201a0880aeb751085a354a3d657622a29c222
hadiwq <98136543+hadiwq@users.noreply.github.com>
2022-11-08 19:37:54 -0600

    Update index.php

--- index.php
+++ index.php
@@ -39,6 +39,24 @@ document.getElementById("demo").innerHTML = Math.PI;
   Math.random();
   
   }
+    
+    <html>
+<body>
+
+<h2>JavaScript Math</h2>
+
+<p>Math.floor(Math.random() * 10) returns a random integer between 0 and 9 (both 
+included):</p>
+
+<p id="demo"></p>
+
+<script>
+document.getElementById("demo").innerHTML =
+Math.floor(Math.random() * 10);
+</script>
+
+</body>
+</html>
 
 </body>
 </html> 
