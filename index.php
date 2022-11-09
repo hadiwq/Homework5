8f0062d9be80ec1584e8ac097d4b00c46b906bf9
Hadi Alqadi <hadi.alqadi@me.com>
2022-11-08 20:51:31 -0600

    2

--- index.php
+++ index.php
@@ -21,42 +21,7 @@ Click me to display Date and Time.</button>
 
 <p id="demo"></p>
 
+<a href="randomnumber.php">Random Number</a>
   
-  
-  <h2>JavaScript Math.PI</h2>
-
-<p>Math.PI returns the ratio of a circle's circumference to its diameter:</p>
-
-<p iid="math"></p>
-
-<script>
-document.getElementById("math").innerHTML = Math.PI;
-</script>
-
-
-  
-  
-  Math.random();
-  
-  
-    
-    <html>
-<body>
-
-<h2>JavaScript Math</h2>
-
-<p>Math.floor(Math.random() * 10) returns a random integer between 0 and 9 (both 
-included):</p>
-
-<p id="demo"></p>
-
-<script>
-document.getElementById("demo").innerHTML =
-Math.floor(Math.random() * 10);
-</script>
-
-</body>
-</html>
-
 </body>
 </html> 
