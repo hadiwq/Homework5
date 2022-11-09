e210cddcb25734110ecd06afc03a3d86b1e79129
Hadi Alqadi <hadi.alqadi@me.com>
2022-11-08 20:43:33 -0600

    commit

---
+++ randomnumber.php
@@ -0,0 +1,17 @@
+<html lang="en">
+  
+  <body>
+<h2>JavaScript Math.PI</h2>
+
+<p>Math.PI returns the ratio of a circle's circumference to its diameter:</p>
+
+<p iid="math"></p>
+
+<script>
+document.getElementById("math").innerHTML = Math.PI;
+</script>
+
+</body>
+</html>
+
+]
